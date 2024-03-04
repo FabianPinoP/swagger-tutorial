452c8ab49f54a9c0e054897cbcc6d0e330204511
FabianPinoP <fpinoperez@outlook.cl>
2024-03-04 14:11:11 -0300

    change url swagger and added healt endpoint

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ swagger(app)
 app.use(express.json());
 app.options('*', cors())
 app.use(logger());
+app.get("/", (req, res) => {
+  res.send("api en linea");
+});
 app.use("/api/v1", userRoutes);
 app.use("/api/v1", travelsRouter);
 app.use("/api/v1", loginRoutes);
@@ -21,6 +24,7 @@ app.use("/api/v1", loginRoutes);
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
   console.log(`Swagger docs available at http://localhost:${PORT}/api/v1/docs`);
+  console.log(`Swagger production documents available at https://swagger-tutorial.onrender.com/api/v1/docs`);
 });
 
 export default app;
